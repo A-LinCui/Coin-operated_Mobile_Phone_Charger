a8c1c6eb3158dee137a424e07c5f7236997ed9ba
Junbo Zhao <zhaojb17@mails.tsinghua.edu.cn>
2020-12-23 12:37:07 +0800

    Update Keyboard_Scanner.v

--- component/Keyboard_Scanner.v
+++ component/Keyboard_Scanner.v
@@ -1,5 +1,6 @@
-// Keyboard
+// Keyboard Scanner
 // Author: Junbo Zhao <zhaojb17@mails.tsinghua.edu.cn>.
+// State: Finished. Passed all test.
 
 module Keyboard_Scanner(
     input clk, //1000Hz after reduction
@@ -10,55 +11,59 @@ module Keyboard_Scanner(
     output reg press //The signal that a key is pressed, passed to ChargeController
     );
     
-    reg [2:0]current_state, next_state;
+    reg [2:0]current_state;
     reg [5:0]cnt;
 
     parameter MAX = 4'b1111; //Stabilization. Valid if no change in 15 ms
-    parameter S0 = 3'b000, S1 = 3'b001, S2 = 3'b010, S3 = 3'b011, S4=3'b100, S5=3'b101;
     parameter no_press = 4'b1111; //High voltage if not pressed
-
-    always@(posedge clk or posedge rst_n)
-    begin
-        if (rst_n) begin
-            cnt = 0;
-            current_state = S0;
-        end
-        else begin
-            current_state = next_state;
-            if (current_state != S5) cnt = 0;
-            else if (cnt < MAX) cnt = cnt + 1;
-        end
-    end
+    parameter S0 = 3'b000, S1 = 3'b001, S2 = 3'b010, S3 = 3'b011, S4=3'b100, S5=3'b101; //All possible states
 
     always@(posedge clk)
     begin
+	if (rst_n) begin
+	    col = 4'b0000;
+	    cnt = 0;
+	    press = 0;
+	    key_value = 4'b0000;
+	    current_state = S0;
+	end
         case(current_state)
-            S0: next_state = (row == no_press)? S0:S1;
-            S1: next_state = (row == no_press)? S2:S5;
-            S2: next_state = (row == no_press)? S3:S5;
-            S3: next_state = (row == no_press)? S4:S5;
-            S4: next_state = (row == no_press)? S0:S5;
-            S5: next_state = (row == no_press)? S0:S5;
-        endcase
-    end
-
-    always@(current_state or cnt or rst_n) begin
-        if (rst_n) begin
-            col = 4'b0000;
-            press = 0;
-            key_value = 4'b0000;
-        end
-        case(current_state)
-            S0: begin
-                col = 4'b0000;
-		        press = 0;
-		        key_value = 4'b0000;
+            S0: if (row == no_press) begin
+		    col = 4'b0000;
+		    press = 0;
+                    cnt = 0;
+                    key_value = 4'b0000;
+		end
+                else begin
+                    col = 4'b0111;
+                    current_state = S1;
                 end
-            S1: col = 4'b0111;
-            S2: col = 4'b1011;
-            S3: col = 4'b1101;
-            S4: col = 4'b1110;
-            S5: if(cnt == MAX) begin
+            S1: if(row == no_press) begin
+                    col = 4'b1011;
+                    current_state = S2;
+                end
+                else current_state = S5;
+            S2: if(row == no_press) begin
+                    col = 4'b1101;
+                    current_state = S3;
+                end
+                else current_state = S5;
+            S3: if(row == no_press) begin
+                    col = 4'b1110;
+                    current_state = S4;
+                end
+                else current_state = S5;
+            S4: if(row == no_press) begin
+                    col = 4'b0000;
+                    current_state = S0;
+                end
+                else current_state = S5;
+            S5: if(row == no_press) begin
+                    current_state = S0;
+                    col = 4'b0000;
+                end
+                else if (cnt < MAX) cnt = cnt + 1;
+                else begin
                     case({col, row})
                         8'b01110111: begin
                             key_value = 4'b0001; //1
